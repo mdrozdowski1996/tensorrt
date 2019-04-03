03f20ada9eef3368250117a59841ebaeec4a4659
Marek Drozdowski <mdrozdowski@nvidia.com>
2019-04-03 16:10:24 -0700

    add ncf inference script

---
+++ tftrt/examples/ncf/inference.py
@@ -0,0 +1,166 @@
+import tensorflow as tf
+import tensorflow.contrib.tensorrt as trt
+import time
+import random
+import numpy as np
+from official.dataset import movielens
+
+
+from neumf import ncf_model
+from neumf import NeuMF
+import os
+import argparse
+import csv
+
+
+def get_frozen_graph(model_checkpoint="/data/marek_ckpt/model.ckpt",
+                     model_dtype=tf.float32,
+                     mf_dim=64,
+                     mf_reg=64,
+                     mlp_layer_sizes=[256, 256, 128, 64],
+                     mlp_layer_regs=[.0, .0, .0, .0],
+                     nb_items=26744,
+                     nb_users=138493):
+    tf_config = tf.ConfigProto()
+    with tf.Graph().as_default() as tf_graph:
+        with tf.Session(config=tf_config) as tf_sess:
+            users = tf.placeholder(shape=(None,), dtype=tf.int32, name="user_input")
+            items = tf.placeholder(shape=(None,), dtype=tf.int32, name="item_input")
+            with tf.variable_scope("neumf"):
+                logits = NeuMF(users, items, model_dtype, nb_users, nb_items, mf_dim, mf_reg, mlp_layer_sizes, mlp_layer_regs)
+            saver = tf.train.Saver()
+            saver.restore(tf_sess, "/data/marek_ckpt/model.ckpt")
+            graph0 = tf.graph_util.convert_variables_to_constants(tf_sess,
+                tf_sess.graph_def, output_node_names=['neumf/dense_3/BiasAdd'])
+            frozen_graph = tf.graph_util.remove_training_nodes(graph0)
+
+            for node in frozen_graph.node:
+                if node.op == "Assign":
+                    node.op = "Identity"
+                    if 'use_locking' in node.attr: del node.attr['use_locking']
+                    if 'validate_shape' in node.attr: del node.attr['validate_shape']
+                    if len(node.input) == 2:
+                        node.input[0] = node.input[1]
+                        del node.input[1]
+    return frozen_graph
+
+def optimize_model(frozen_graph, 
+                   use_trt=True,
+                   precision_mode="FP16",
+                   batch_size=128):
+    if use_trt:
+        trt_graph = trt.create_inference_graph(frozen_graph, ['neumf/dense_3/BiasAdd:0'], max_batch_size=batch_size, precision_mode=precision_mode)
+    return trt_graph
+
+def input_data(use_synthetic=True,
+               batch_size=128,
+               data_dir=None,
+               num_iterations=None,
+               nb_items=26744,
+               nb_users=1388493):
+    
+    if use_synthetic and num_iterations is None:
+        num_iterations = 10000
+
+    if use_synthetic:
+        items = [[random.randint(1, nb_items) for _ in range(batch_size)] for _ in range(num_iterations)]
+        users = [[random.randint(1, nb_users) for _ in range(batch_size)] for _ in range(num_iterations)]
+    else:
+        if os.path.exists(data_dir):
+             print("Using cached dataset: %s" % (data_dir))
+        else:
+             data_path = os.path.join(data_dir, 'test_ratings.pickle')
+             dataset = pd.read_pickle(data_path)
+             users = dataset["user_id"]
+             items = dataset["item_id"]
+             #movielens.download("ml-20m", data_dir)
+        
+        
+        #with open(data_dir + 'ratings.csv') as csv_file:
+        #    ratings_reader = csv.reader(csv_file, delimiter=',')
+        #with open(data_dir + 'movies.csv') as csv_file:
+        #    movies_reader = csv.reader(csv_file, delimiter=',')
+        #with open(data_dir + 'users.csv') as csv_file:
+        #    users_reader = csv.reader(csv_file, delimiter=',')
+        #with open(data_dir + '')
+             
+    return items, users
+
+
+def run_inference(frozen_graph,
+                  use_synthetic=True,
+                  batch_size=128,
+                  data_dir=None,
+                  num_iterations=10000,
+                  num_warmup_iterations=2000,
+                  nb_items=26744,
+                  nb_users=1388493,
+                  display_every=100):
+
+    items, users = input_data(use_synthetic,
+                              batch_size,
+                              data_dir,
+                              num_iterations,
+                              nb_items,
+                              nb_users)
+
+    with tf.Graph().as_default() as tf_graph:
+        with tf.Session(config=tf.ConfigProto()) as tf_sess:
+            tf.import_graph_def(frozen_graph, name='')
+            output = tf_sess.graph.get_tensor_by_name('neumf/dense_3/BiasAdd:0')
+            runtimes = []
+            res = []
+
+            for n in range(num_iterations):
+                item = items[n]
+                user = users[n]
+
+                beg = time.time()
+                r = tf_sess.run(output, feed_dict={'item_input:0': item, 'user_input:0': user})
+                end = time.time()
+
+                res.append(r)
+                runtimes.append(end-beg)
+                if n % display_every == 0:
+                    print("    step %d/%d, iter_time(ms)=%.4f" % (
+                        len(runtimes),
+                        num_iterations,
+                        np.mean(runtimes[(-1)*display_every]) * 1000))
+            
+            print("throghput: %.1f" % 
+                (batch_size * num_iterations/np.sum(runtimes)))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Evaluate model')
+    parse.add_argument('--use_synthetic', type=str, default=None)
+    parser.add_argument('--data_dir', type=str, default=None,
+            help='Directory containing validation set csv files.')
+    parser.add_argument('--model_dir', type=str, default=None,
+            help='Directory containing model checkpoint.')
+    parser.add_argument('--use_trt', action='store_true',
+            help='If set, the graph will be converted to a TensorRT graph.')
+    parser.add_argument('--precision', type=str, choices=['fp32', 'fp16', 'int8'], default='fp32',
+            help='Precision mode to use. FP16 and INT8 only work in conjunction with --use_trt')
+    parser.add_argument('--nb_items', type=int, default=26744,
+            help='Number of items') 
+    parser.add_argument('--nb_users', type=int, default=1388493,
+            help='Number of users')
+    parser.add_argument('--batch_size', type=int, default=8,
+            help='Batch size')
+    #parser.add_argument('--model_dtype', , default=tf.float32) 
+    parser.add_argument('--mf_dim', type=int, default=64)
+    parser.add_argument('--mf_reg', type=int, default=64)
+    parser.add_argument('--mlp_layer_sizes', default=[256, 256, 128, 64])
+    parser.add_argument('--mlp_layer_regs', default=[.0, .0, .0, .0])
+
+    args = parser.parse_args()
+    if not args.use_synthetic and args.data_dir:
+        raise ValueError("Data_dir is not provided")
+
+    frozen_graph = get_frozen_graph()
+
+    frozen_graph = optimize_model(frozen_graph)
+
+    run_inference(frozen_graph)
+
